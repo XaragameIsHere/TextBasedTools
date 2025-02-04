1c55e06ffb455024276fe046076587efaf76c1a6
Xara Shaw <xaragamens30@gmail.com>
2025-02-04 12:55:31 -0800

    First commit

---
+++ Assets/Scripts/dialogueLibrary.cs
@@ -0,0 +1,166 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
+
+
+public class dialogueLibrary : MonoBehaviour
+{
+
+    public bool dev_SkipCutscene = false;
+    private DialogueParsing.DialogueData mainData;
+
+	public RawImage dialogueChoiceBox;
+    public Slider patienceMeter;
+    public List<TMP_Text> choiceButtons;
+
+    public Image dialoguePlayer;
+
+	public TMP_Text playerText;
+    [SerializeField] float dialogueSpeed = .02f;
+    [SerializeField] Image playerTextEnter;
+    [SerializeField] float camSize;
+    [SerializeField] UnityEvent functions;
+    
+
+    public IEnumerator Dialogue(DialogueParsing.Choice choice)
+	{
+		foreach (DialogueParsing.DialogueLine line in choice.dialogueLines)
+		{
+            //functions.
+            for (int i = 1; i <= line.dialogueText.Length; i++)
+            {
+                playerText.text = line.dialogueText.Substring(0, i);
+                
+
+                yield return new WaitForSeconds(dialogueSpeed);
+            }
+            playerTextEnter.enabled = true;
+            playerText.text = line.dialogueText;
+			
+            yield return new WaitUntil(() => Input.GetButtonDown("Submit"));
+            playerTextEnter.enabled = false;
+
+			playerText.text = "";
+		}
+		stopDialogue();
+	}
+
+    
+    
+    private bool isClicked = false;
+    int clickedButton;
+    public void click(int s) 
+    {
+        clickedButton = s;
+        isClicked = true;
+    }
+
+    /*
+     
+    Important, stuff for typewriter effect
+    
+    
+    
+    private Coroutine dialogueRoutine;
+    private IEnumerator typeWrite(DialogueParsing.Dialogue dialogueRoot, DialogueParsing.Selection line)
+    {
+
+        
+
+        //print("finished typewriting "+ line.enemy_Text);
+        yield return new WaitUntil(() => Input.GetButtonDown("Submit"));
+        //print("finished creating choices " );
+        for (int i = 0; i < 4; i++)
+        {
+            choiceButtons[i].text = line.choices[i].dialogueLine;
+        }
+
+        dialogueChoiceBox.transform.DOLocalMoveY(-300, 1);
+        isClicked = false;
+        //print("moving box of choices" );
+        yield return new WaitUntil(() => isClicked);
+        isClicked = false;
+        //print("player has chosen");
+        dialogueChoiceBox.transform.DOLocalMoveY(-817, 1);
+
+        for (int i = 1; i <= line.choices[clickedButton].dialogueLine.Length; i++)
+        {
+            playerText.text = line.choices[clickedButton].dialogueLine.Substring(0, i);
+
+
+            yield return new WaitForSeconds(dialogueSpeed);
+        }
+
+        //print("finished typewriting " + line.choices[clickedButton].dialogueLine);
+        yield return new WaitUntil(() => Input.GetButtonDown("Submit"));
+        //print("moving on");
+
+        if (line.choices[clickedButton].next_Dialogue_Selection == "end")
+        {
+            patienceMeter.transform.DOLocalMoveX(-1309, 1);
+            StartCoroutine( dialogue(mainData.End));
+        }
+        else
+        {
+            navigateToSelection(dialogueRoot, line.choices[clickedButton].next_Dialogue_Selection);
+        }
+            
+        
+    }
+*/
+    public void navigateToSelection(DialogueParsing.Dialogue dialogueRoot, string nameOfSelection)
+    {
+        print(nameOfSelection);
+        patienceMeter.transform.DOLocalMoveX(-714, 1);
+        dialoguePlayer.transform.DOLocalMoveX(-880, 1);
+
+
+
+        foreach (DialogueParsing.Selection data in dialogueRoot.actionSelections)
+        {
+
+			if (data.name == nameOfSelection)
+            {
+                //print(data.Name);
+                
+                //dialogueRoutine = StartCoroutine(typeWrite(dialogueRoot, data));
+            }
+
+        }
+
+    }
+
+    /*
+    private float patience = 5;
+    public IEnumerator moveMeter()
+    {
+        patienceMeter.value = patience / 10;
+        while (patience > 0)
+        {
+            yield return new WaitForSeconds(3);
+            patience -= 1;
+            patienceMeter.value = patience/10;
+        }
+        StopCoroutine(dialogueRoutine);
+
+        stopDialogue();
+    }*/
+    
+    private void stopDialogue()
+	{
+        print("end");
+        /*
+        patienceMeter.transform.DOLocalMoveX(-1309, 1);
+        dialoguePlayer.transform.DOMoveX(-1712, 1);
+        dialogueEnemy.transform.DOMoveX(-1712, 1);
+        dialogueChoiceBox.transform.DOLocalMoveY(-781, 1);
+        zoomIn.transform.DOLocalMoveY(1635, 1);
+        */
+        
+	}
+
+}
---
+++ Assets/Scripts/interfaceComponent.cs
@@ -0,0 +1,51 @@
+
+/// <summary>
+/// this is the componenent of the UI that controls the dialogue in game
+/// ye
+/// </summary>
+/// 
+[System.Serializable]
+public class DialogueParsing
+{
+    [System.Serializable]
+    public class DialogueLine
+    {
+        public string sprite;
+        public string dialogueText;
+    }
+
+
+    [System.Serializable]
+    public class DialogueData
+    {
+        public DialogueLine[] start;
+        public DialogueLine[] end;
+    }
+
+    [System.Serializable]
+    public class Choice
+    {
+        public string reactionText;
+        public string[] reaction;
+        public DialogueLine[] dialogueLines;
+        public string nextDialogueSelection;
+    }
+
+    [System.Serializable]
+    public class Selection
+    {
+        public string name;
+        public string context;
+        public Choice[] choices;
+    }
+
+    [System.Serializable]
+    public class Dialogue
+    {
+        public DialogueData[] cutsceneDialogue;
+        public Selection[] actionSelections;
+    }
+
+
+}
+
